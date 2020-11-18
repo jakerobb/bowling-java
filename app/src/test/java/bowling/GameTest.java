d53f94385558516e156cd9cdf16c561d842c1a97
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2020-11-18 11:34:27 +0100

    Update test function name.

--- app/src/test/java/bowling/GameTest.java
+++ app/src/test/java/bowling/GameTest.java
@@ -4,9 +4,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class GameTest {
-    @Test public void testAppHasAGreeting() {
+    @Test public void testGutterGame() {
         Game classUnderTest = new Game();
         classUnderTest.roll(0);
+        classUnderTest.roll(0);
+        classUnderTest.roll(0);
+
         assertEquals(0, classUnderTest.score());
     }
 }
